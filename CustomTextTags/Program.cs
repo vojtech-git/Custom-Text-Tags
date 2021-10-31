37ef80125216168885403703c6ba14a80c647369
Vojtech Pejsar <pejsarvojtech@gmail.com>
2021-10-31 17:14:44 +0100

    nejaké úpravy nevím kdy je idealní ukládat tak ukládám teď

--- CustomTextTags/Program.cs
+++ CustomTextTags/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Diagnostics;
+using System.Text.RegularExpressions;
 
 namespace CustomTextTags
 {
@@ -12,12 +13,47 @@ namespace CustomTextTags
     // potom si ulozim string mezi jejich pozicema a pak v původnim textu dam proste Replace() ty strigy co jsem nasel mezi tagama
     // jaký tag jsem nasel a co je mezi nim za string si ulozim do dictionary a potom proste budu mit metodu ReplaceStrings()
 
+    // ok existuje Regex.Match() což vrací ten matchující string to řeší hodně
+
+    public delegate string ReplaceString(string textToEdit);
+
     class Program
-    {
-        static string userInput;
+    {        
+        public static Tag[] tags = new Tag[] { new Tag { tagName = "!", replaceLogic = new ReplaceString(replaceLogicExclamaitionMark) } }; // názvy tagů
+
         static void Main(string[] args)
         {
-            userInput = Console.ReadLine();
+            string userInput = Console.ReadLine();
+            //Edit(userInput);
+
+            Console.WriteLine(Edit(userInput));
+            Console.ReadLine();
         }
+
+        public static string Edit(string textToEdit)
+        {
+            Regex rgx;
+            string pattern = @"^<"; //^<> <\/>
+
+            foreach (Tag tag in tags)
+            {
+                pattern += tag.tagName + @"> <\";
+                pattern += tag.tagName + @">";
+                rgx = new Regex(pattern);
+
+                Match match = rgx.Match(textToEdit);
+
+                tag.replaceLogic(match.Value);
+            }
+
+            return "";
+        }
+
+        #region replace metohods
+        public static string replaceLogicExclamaitionMark(string textToReplace)
+        {
+            return $"!!{textToReplace.ToUpper()}!!";
+        }
+        #endregion
     }
 }
---
+++ CustomTextTags/Tag.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomTextTags
+{
+    class Tag
+    {
+        public string tagName;
+
+        public ReplaceString replaceLogic;
+
+        public string Replace(string stringToReplace) // logika stejná pro všechny tagy
+        {
+            stringToReplace.Replace($"<{tagName}>", "");
+            stringToReplace.Replace($"</{tagName}>", "");
+
+            replaceLogic(stringToReplace);
+
+            return stringToReplace;
+        }
+    }
+}
